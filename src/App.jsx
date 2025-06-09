17b2d196043f20b5aff094f910428776fe44a032
yujin-zero <poly30@naver.com>
2025-06-09 12:58:07 +0900

    init

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+// import HelloWorld from "./components/HelloWorld";
+// import { useState } from "react";
+// import CaptionImage from "./components/CaptionImage";
+// import CountComponent from "./components/CountComponent";
+// import MyButton from "./components/MyButton";
+// import MyToggleButton from "./components/MyToggleButton";
+// import ForbidFilter from "./components/ForbidFilter";
+// import ExampleUseState from "./components/ExampleUseState";
+// import DataRendering from "./components/DataRendering";
+// import StopWatch from "./components/StopWatch";
+// import Timer from "./components/Timer";
+// import TodoApp from "./components/Todo-App";
+// import FocusInputButton from "./components/FocusInputButton";
+// import PrimeCalculator from "./components/PrimeCalculator";
+// import MemoExample from "./components/MemoExample";
+import CallbackExample from "./components/CallbackExample/CallbackExample";
+
+function App() {
+  // const [visible, setVisible] = useState(false);
+  return (
+    // <div>
+    //   <button onClick={() => setVisible(!visible)}>보이기</button>
+    //   {visible ? <CountComponent /> : null}
+    // </div>
+    <div>
+      <CallbackExample />
+    </div>
+  );
+}
+
+export default App;
+
+// // <div style={{ textAlign: "center" }}>
+//   {/* <CaptionImage
+//     imgUrl="https://lh4.googleusercontent.com/proxy/mNF7yCR1K7wbRUZ-RM7GXoNNrOCTW_tPEpVGtIftOCususB2__CpjC7cUm2QW15Pqya6WULx_wI1dbR4xUHkXDYqAzsRz040qhTG6I0p"
+//     caption="이것이 트럭이다."
+//   />
+//   <MyButton
+//     title="네이버로 이동"
+//     clickUrl="https://www.naver.com"
+//     color="green"
+//   /> */}
+//   {/* <CountComponent /> */}
+//   {/* <BilnkComponent /> */}
+// // </div>
