a61a5abc018449de46ca69566c6792869509ddf4
yujin-zero <poly30@naver.com>
2025-06-13 10:10:42 +0900

    init: [myapp] express 프로젝트 초기 설정

---
+++ myapp/routes/board.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", function (req, res, next) {
+  console.log("router /board 실행");
+  res.send("This is Board입니다.");
+});
