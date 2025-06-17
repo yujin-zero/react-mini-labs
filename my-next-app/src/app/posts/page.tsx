ac2ba2629afa58b72219e7cf9d85944243ea7b42
yujin-zero <poly30@naver.com>
2025-06-17 11:07:34 +0900

    add: exam2- App router

---
+++ my-next-app/src/app/posts/page.tsx
@@ -0,0 +1,12 @@
+export default function PostListPage() {
+	return (
+		<div>
+			<h1 className="text-4xl font-bold">포스트 리스트</h1>
+			<ul>
+				<li>게시글1</li>
+				<li>게시글2</li>
+				<li>게시글3</li>
+			</ul>
+		</div>
+	);
+}
