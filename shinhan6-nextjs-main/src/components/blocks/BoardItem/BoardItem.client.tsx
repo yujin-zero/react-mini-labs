edee0dd277b1679c192dde484de8c185dd33fb5f
yujin-zero <poly30@naver.com>
2025-06-23 09:30:28 +0900

    feat: [next-app] 게시글 상세 페이지에 댓글 작성 기능 추가

--- shinhan6-nextjs-main/src/components/blocks/BoardItem/BoardItem.client.tsx
+++ shinhan6-nextjs-main/src/components/blocks/BoardItem/BoardItem.client.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { Container, Button, Form, Row, Col, Card } from "react-bootstrap";
+import { Container, Button, Form, Row, Card } from "react-bootstrap";
 import { useRouter } from "next/navigation";
 
 type Board = {
@@ -25,6 +25,7 @@ export default function BoardItemClient({ board }: BoardItemClientProps) {
   const [content, setContent] = useState("");
   const router = useRouter();
   const [comments, setComments] = useState<Comment[]>([]);
+  const [text, setText] = useState("");
 
   const deleteBoard = () => {
     fetch(`http://localhost:4001/board/${board._id}`, {
@@ -53,6 +54,25 @@ export default function BoardItemClient({ board }: BoardItemClientProps) {
       });
   }, []);
 
+  const postComment = async (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("댓글등록");
+
+    const response = await fetch("http://localhost:4001/comment/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ boardId: board._id, content: text }),
+    });
+
+    if (response.ok) {
+      const newComment = await response.json();
+      setComments((prev) => [...prev, newComment]);
+      setText("");
+    }
+  };
+
   return (
     <Container className="pt-5">
       <h2 className="mb-4">게시글 조회</h2>
@@ -78,6 +98,7 @@ export default function BoardItemClient({ board }: BoardItemClientProps) {
       </Form>
 
       <h3 className="mt-4">댓글</h3>
+
       {comments.map((comment) => (
         <Row key={comment._id} className="g-3">
           <Card className="border rounded-3 h-100">
@@ -89,6 +110,17 @@ export default function BoardItemClient({ board }: BoardItemClientProps) {
           </Card>
         </Row>
       ))}
+      <Form onSubmit={postComment} className="mt-5">
+        <Form.Label>댓글 작성</Form.Label>
+        <Form.Control
+          value={text}
+          onChange={(e) => setText(e.target.value)}></Form.Control>
+        <div className="d-flex justify-content-end mt-3">
+          <Button variant="outline-info" type="submit">
+            댓글 등록
+          </Button>
+        </div>
+      </Form>
     </Container>
   );
 }
